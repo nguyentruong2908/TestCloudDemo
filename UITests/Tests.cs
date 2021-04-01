56c992bf8bc3da672264baa01e00aba970888992
congacippro113@gmail.com <congacippro113@gmail.com>
2021-04-01 21:56:09 +0700

    Add project files.

---
+++ UITests/Tests.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+
+namespace TestCloudDemo.UITests
+{
+    [TestFixture (Platform.Android)]
+    [TestFixture (Platform.iOS)]
+    public class Tests
+    {
+        IApp app;
+        Platform platform;
+
+        public Tests (Platform platform)
+        {
+            this.platform = platform;
+        }
+
+        [SetUp]
+        public void BeforeEachTest ()
+        {
+            app = AppInitializer.StartApp (platform);
+        }
+
+        [Test]
+        public void AppLaunches ()
+        {
+            app.Screenshot ("First screen.");
+        }
+
+        [Test]
+        public void Press_Good_Button_And_Pass_Hooray ()
+        {
+            // Arrange
+            // Nothing to arrange for this test
+
+            // Act
+            app.Tap (e => e.Marked ("SucceedButton"));
+            app.Screenshot ("Green button tapped");
+
+            // Assert
+            Assert.AreEqual ("Hooray!", app.Query (e => e.Marked ("ResultLabel")).First ().Text);
+        }
+
+        [Test]
+        public void Press_Bad_Button_And_Fail_Boo ()
+        {
+            // Arrange
+            // Nothing to arrange for this test
+
+            // Act
+            app.Tap (e => e.Marked ("FailButton"));
+            app.Screenshot ("Red button tapped");
+
+            Assert.AreEqual ("Whoops, that is embarrassing...", app.Query (e => e.Marked ("ResultLabel")).First ().Text);
+        }
+
+    }
+}
---
+++ iOS/AppDelegate.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Foundation;
+using UIKit;
+using Xamarin.Forms;
+
+namespace TestCloudDemo.iOS
+{
+    [Register ("AppDelegate")]
+    public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
+    {
+        public override bool FinishedLaunching (UIApplication app, NSDictionary options)
+        {
+            global::Xamarin.Forms.Forms.Init ();
+
+            Forms.ViewInitialized += (object sender, ViewInitializedEventArgs e) => {
+                if (!string.IsNullOrWhiteSpace (e.View.AutomationId)) {
+                    e.NativeView.AccessibilityIdentifier = e.View.AutomationId;
+                }
+            };
+
+            // Code for starting up the Xamarin Test Cloud Agent
+#if ENABLE_TEST_CLOUD
+            Xamarin.Calabash.Start ();
+#endif
+
+            LoadApplication (new App ());
+
+            return base.FinishedLaunching (app, options);
+        }
+    }
+}
